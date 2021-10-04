83a1af5c78df053ea8ee1b2ad598d4f0d99aa626
joao-svalencar <joaopaulo.valencar@usp.br>
2021-10-04 20:51:44 -0300

    added data for elevation test

--- R/C_tets.R
+++ R/C_tets.R
@@ -6,9 +6,19 @@ library(rgdal)
 
 # reading processed data: cerrado endemic terrestrial vetebrates ----------
 
+
+# For Biotic Element Analysis ---------------------------------------------
+
 pa_table <- read.csv(here("data", "sppsample.csv"))
 centro <- read.csv(here("data", "coords.csv"), header=TRUE)
 dd <- readOGR(dsn=here("data","shapes"), layer= ("grid_tet"))
 s <- readOGR(dsn=here("data", "shapes"), layer="Cerrado") #study area shapefile
+
+# For the linear model (elevation) ----------------------------------------
+list <- read.csv(here("data", "list.csv"))
+db <- read.csv(here("data", "baseunique_alt.csv"), stringsAsFactors=FALSE, fileEncoding="latin1")
+db_full <- read.csv(here("data", "BD_endemics.csv"), stringsAsFactors=FALSE, fileEncoding="latin1")
+be <- read.csv(here("outputs", "tables", "spp_hier_2_015.csv"))
+comp <- read.csv(here("outputs", "tables", "comp.csv"))
 
 # end ---------------------------------------------------------------------
