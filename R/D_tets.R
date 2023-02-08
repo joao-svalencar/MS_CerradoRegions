4ab96eae31284d949644d2f821ec05b0aa65afc0
joao-svalencar <joaopaulo.valencar@gmail.com>
2023-02-07 23:16:30 -0300

    species elevation class - Kruskal-Wallis

--- R/D_tets.R
+++ R/D_tets.R
@@ -44,13 +44,13 @@ head(db_be) #unique records, with elevation, BEs and list information
 db_be <- merge(db_be, be, by="species") #add BEs info to dataframe
 head(db_be)
 
-comp <- comp[, c(1, 14:15)]
-head(comp)
+#comp <- comp[, c(1, 14:15)]
+#head(comp)
 
-db_be <- merge(db_be, comp, by="BEs") #combine with info or BE range classification AND REMOVE NOISE COMPONENT SPECIES RECORDS
-head(db_be)
+#db_be <- merge(db_be, comp, by="BEs") #combine with info or BE range classification AND REMOVE NOISE COMPONENT SPECIES RECORDS
+#head(db_be)
 
-str(db_be)
+#str(db_be)
 
 db_be$BEs <- as.factor(db_be$BEs) #BEs into factors
 db_be$BEs <- relevel(db_be$BEs, "2") #BE 2 (widespread) as basal level
@@ -59,8 +59,58 @@ summary(db_be$elevation) #summary with unique records
 #Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
 #0.0   376.0   611.0   624.6   861.0  2067.0 
 
-# linear models -----------------------------------------------------------
 
+# Species elevation classification ----------------------------------------
+
+list$elev_class <- NA
+spp_krusk <- names(which(table(db_be$species)>5)) #selecting species with more than 5 records
+
+for(i in 1:length(spp_krusk))
+{  
+  spp <- spp_krusk[i] #creating object with species name
+  spp_el <- db_be$elevation[db_be$species==spp] #selecting the elevation values for a given species 'i'
+  #el_sample <- sample(db_be$elevation, 1000, replace=FALSE) #sampling random elevation values from our database
+  el_sample <- sample(db_be$elevation, 1000, replace=TRUE) #sampling random elevation values from our database
+  el_ktest <- c(spp_el, el_sample) #creating a vector combining a given species 'i' and the 1,000 random elevation values
+  group <- rep(c("spp", "r.sample"), times=c(length(spp_el), 1000)) #creating a vector to group the species records and the random elevation values
+  k.test <- data.frame(el_ktest, group) #creating a dataframe with elevation values and group categories
+  kruskal <- kruskal.test(el_ktest~group, data=k.test) #comparing species elevation values and random elevation values
+  
+  class <- if(summary(db_be$elevation[db_be$species==spp])[2]>500)
+              {
+                "plateau"
+              }else if(summary(db_be$elevation[db_be$species==spp])[5]<500)
+                  {
+                "depression"
+                  }else if(kruskal$p.value<0.05)
+                    {
+                      if(tapply(k.test$el_ktest, k.test$group, median)[1]<tapply(k.test$el_ktest, k.test$group, median)[2])
+                      {
+                      "plateau"
+                      }else("depression")
+                  }else("general")
+  
+  list$elev_class[list$species==spp] <- class
+  boxplot(k.test$el_ktest~k.test$group, xlab=class, ylab="Elevation", main=spp)
+}
+
+table(list$elev_class)
+list
+
+sd(db_be$elevation[db_be$species==spp])-summary(db_be$elevation[db_be$species==spp])[2]
+
+sd(db_be$elevation[db_be$species==spp])-summary(db_be$elevation[db_be$species==spp])[5]
+
+summary(db_be$elevation[db_be$species==spp])[1]-summary(db_be$elevation[db_be$species==spp])[3]
+summary(db_be$elevation[db_be$species==spp])[5]-summary(db_be$elevation[db_be$species==spp])[3]
+
+summary(db_be$elevation[db_be$species==spp])
+
+# -------------------------------------------------------------------------
+
+
+
+# linear models -----------------------------------------------------------
 # mod1 - elevation_full ---------------------------------------------------
 
 mod1 <- lm(elevation~BEs, data=db_be) #basal level is BE 2 the widespread BE
@@ -88,7 +138,6 @@ dev.off()
 png(here("outputs", "tests","RestrictedResidualsHist.png"), width=6, height=6, units='in', res=300)
 hist(mod2$residuals)
 dev.off()
-
 
 # Open S_boxplot.R --------------------------------------------------------
 
